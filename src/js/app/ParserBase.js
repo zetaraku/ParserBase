7d7181a42738b2abfd7b35c0257161eeba8b4948
Raku Zeta <zetaraku@gmail.com>
2018-05-26 23:59:44 +0800

    remove 'use strict'

--- src/js/app/ParserBase.js
+++ src/js/app/ParserBase.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const _ext = require('./_ext');
 const {
 	GSymbol,
--- src/js/app/main.js
+++ src/js/app/main.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const $ = require('jquery');
 const ParserBase = require('./ParserBase');
 const main_functions = require('./main_functions');
